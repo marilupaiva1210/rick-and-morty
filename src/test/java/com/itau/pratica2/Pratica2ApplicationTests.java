bf627abf704972e99521390b554469fcda919a77
marilupaiva <marilupaiva@brq.com>
2023-09-29 15:00:09 -0300

    commit

---
+++ src/test/java/com/itau/pratica2/Pratica2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.pratica2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pratica2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
